801b1b3c5997ff31f7d33c9a0b0d5d7697f526eb
Siddhant-Mahato <siddhantmahato252@gmail.com>
2024-07-24 18:13:38 +0530

    Using Swr Fetching the data from API in client side

--- src/app/client-data-fetch/page.js
+++ src/app/client-data-fetch/page.js
@@ -1,73 +1,84 @@
 'use client'
 
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 // over here use up  useEffect Hook , swr , useSwr with loading state
 
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 function ClientSideDataFetching() {
 
-    const [loading, setLoading] = useState(false);
+    // const [loading, setLoading] = useState(false);
 
-    const [users, setUser] = useState([]);
+    // const [users, setUser] = useState([]);
 
 
-    async function fetchListOfUsers() {
-        try
-        {
-            setLoading(true);
+    // async function fetchListOfUsers() {
+    //     try
+    //     {
+    //         setLoading(true);
 
-            const response = await fetch("https://dummyjson.com/users");
-            const data = await response.json();
+    //         const response = await fetch("https://dummyjson.com/users");
+    //         const data = await response.json();
 
-            if (data?.users)
-            {
-                setUser(data.users);
-                setLoading(false);
-            }
+    //         if (data?.users)
+    //         {
+    //             setUser(data.users);
+    //             setLoading(false);
+    //         }
 
-            return data.users;
-        }
-        catch (error)
-        {
-            throw new Error(error.message);
-            setUser([]);
-            setLoading(false);
-        }
+    //         return data.users;
+    //     }
+    //     catch (error)
+    //     {
+    //         throw new Error(error.message);
+    //         setUser([]);
+    //         setLoading(false);
+    //     }
+    // }
+
+
+
+
+    // useEffect(() => {
+    //     fetchListOfUsers();
+    // }, [])
+
+    const { data, error, isLoading } = useSWR("https://dummyjson.com/users", fetcher);
+
+    if (error) {
+        <div>failed to load</div>;
     }
 
-
-
-
-    useEffect(() => {
-        fetchListOfUsers();
-    }, [])
-    
-
-    if (loading) {
+    if (isLoading) {
         return <h3 className="font-extrabold text-3xl">Loading...</h3>;
     }
 
 return (
     <div className="p-10 ">
         
-            <h1>Client Side Data Fetching : User List Page</h1>
-
-            <ul>
-                {
-                    users && users.length > 0 ?
-                    (users.map((user, index) => (
-                        <li className="mt-5 cursor-pointer" key={index}>
+        <h1>Client Side Data Fetching : User List Page</h1>
+        
+        <ul>
+            {
+                data && data?.users && data?.users.length > 0 ?
+                (data?.users.map((user, index) => (
+                    
+                    <li className="mt-5 cursor-pointer" key={index}>
                             
-                            {/* <Link href={`/client-data-fetch/${user.id}`} >{user.firstName}</Link> */}
+                        {user.firstName}
 
-                            {user.firstName}
+                    </li>
+
+                ))):(null)
+            }
+
+        </ul> 
 
 
-                        </li>
-                    )))
-                    :(null)
-                }
-            </ul>
+
 
         </div>
 );
